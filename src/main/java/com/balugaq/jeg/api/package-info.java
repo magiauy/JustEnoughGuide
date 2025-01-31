a4b08500720549593df56df39f1c6fa618f08083
balugaq <129668183+balugaq@users.noreply.github.com>
2025-01-31 13:00:52 +0800

    Update package-info.java

--- src/main/java/com/balugaq/jeg/api/package-info.java
+++ src/main/java/com/balugaq/jeg/api/package-info.java
@@ -2,7 +2,7 @@ package com.balugaq.jeg.api;
 
 /**
  * This package contains all the API of JEG.
- * Used for compatibility with other libraries.
+ * Used for compatibility with other plugins.
  *
  * @author balugaq
  * @since 1.0
