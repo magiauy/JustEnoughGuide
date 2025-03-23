505762b1d2e67ed27d899f85df31a2f6b1b62ebd
balugaq <129668183+balugaq@users.noreply.github.com>
2025-03-23 10:20:55 +0800

    Update Models.java

--- src/main/java/com/balugaq/jeg/utils/Models.java
+++ src/main/java/com/balugaq/jeg/utils/Models.java
@@ -6,9 +6,9 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class Models {
-    public static final ItemStack RTS_ITEM = Converter.getItem(new SlimefunItemStack("", Converter.getItem(Material.ANVIL, "&b实时搜索", "")));
-    public static final ItemStack SPECIAL_MENU_ITEM = Converter.getItem(new SlimefunItemStack("", Converter.getItem(Material.COMPASS, "&b超大配方", "", "&a点击打开超大配方(若有)")));
-    public static final ItemStack INPUT_TEXT_ICON = Converter.getItem(new SlimefunItemStack("", Converter.getItem(
+    public static final ItemStack RTS_ITEM = Converter.getItem(new SlimefunItemStack("_UI_RTS_ICON", Converter.getItem(Material.ANVIL, "&b实时搜索", "")));
+    public static final ItemStack SPECIAL_MENU_ITEM = Converter.getItem(new SlimefunItemStack("_UI_SPECIAL_MENU_ICON", Converter.getItem(Material.COMPASS, "&b超大配方", "", "&a点击打开超大配方(若有)")));
+    public static final ItemStack INPUT_TEXT_ICON = Converter.getItem(new SlimefunItemStack("_UI_RTS_INPUT_TEXT_ICON", Converter.getItem(
             Material.PAPER,
             "&f搜索: &7在上方输入搜索词",
             "&fNote:",
